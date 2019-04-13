0e2b5724b83425e76d5e7b95cda8c83ab7494f89
Toroishere1995 <ayushsa1995@gmail.com>
2019-04-13 13:38:01 +0530

    Implemented functionality for validity of column name

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IUploadResponse } from '../model/IUploadResponse.model';
 import { ExcelFileService } from '../service/excel-file.service';
-
 @Component({
   selector: 'app-download',
   templateUrl: './download.component.html',
@@ -16,17 +15,19 @@ export class DownloadComponent implements OnInit {
   }
 
   downloadByOrder(myform) {
-    console.log(myform.value['column-order']);
-    this.excelFileService.downloadMergedFile(myform.value['column-order'], this.downloadEvent[0].id,
-      this.downloadEvent[1].id).subscribe(
-        data => {
-          var newBlob = new Blob([data], { type: 'application/vnd.ms-excel' });
-          console.log(data);
-          if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-            window.navigator.msSaveOrOpenBlob(newBlob);
-            return;
-          }
-          const file = window.URL.createObjectURL(newBlob);
+    if (this.orderValid(myform.value['column-order'])==false) {
+
+    } else {
+      this.excelFileService.downloadMergedFile(myform.value['column-order'], this.downloadEvent[0].id,
+        this.downloadEvent[1].id).subscribe(
+          data => {
+            var newBlob = new Blob([data], { type: 'application/vnd.ms-excel' });
+            //   console.log(data);
+            if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+              window.navigator.msSaveOrOpenBlob(newBlob);
+              return;
+            }
+            const file = window.URL.createObjectURL(newBlob);
 
             var link = document.createElement('a');
             link.href = file;
@@ -34,15 +35,35 @@ export class DownloadComponent implements OnInit {
             link.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true, view: window }));
 
             setTimeout(function () {
-                // For Firefox it is necessary to delay revoking the ObjectURL
-                window.URL.revokeObjectURL(file);
-                link.remove();
+              // For Firefox it is necessary to delay revoking the ObjectURL
+              window.URL.revokeObjectURL(file);
+              link.remove();
             }, 100);
-        },
-        err => {
-          console.log(err);
+          },
+          err => {
+            console.log(err);
 
-        }
-      );
+          }
+        );
+    }
+  }
+
+  orderValid(columnHeadingOrder: string) {
+    //console.log(this.downloadEvent[0].columnHeadings);
+    var x: IUploadResponse[] = new Array(2);
+    x = this.downloadEvent.slice();
+
+    var columnFile1 = Array.from(x[0].coloumnHeadings);
+    var columnFile2 = Array.from(this.downloadEvent[1].coloumnHeadings);
+
+    //console.log(columnFile1);
+    //console.log(columnFile2);
+    let children = columnFile1.concat(columnFile2);
+    //console.log(children.sort().join(','));
+
+    var columns = columnHeadingOrder.split(',');
+    //console.log(columns.sort().join(','));
+    //console.log(JSON.stringify(children.sort().join('')) === JSON.stringify(columns.sort().join('')));
+    return JSON.stringify(children.sort().join('')) === JSON.stringify(columns.sort().join(''));
   }
 }
