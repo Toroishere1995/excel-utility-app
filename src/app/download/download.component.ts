d750ea66eef020bc8219d58fad57cf7c8db091cd
Toroishere1995 <ayushsa1995@gmail.com>
2019-04-14 12:58:20 +0530

    Added functionality for error message removal.

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -78,6 +78,7 @@ export class DownloadComponent implements OnInit {
 
   textValue: string = '';
   textAreaEmpty() {
+    this.showMessage=false;
     if (this.textValue.length > 0) {
       this.enableDownload = true;
       console.log(this.textValue);
